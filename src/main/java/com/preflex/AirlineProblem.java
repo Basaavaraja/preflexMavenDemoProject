87bffb654268c9954047fc97e3d38d77e0f3ce08
Foo Bar <foo@bar.com>
2020-01-09 12:30:43 +0530

    TestDemo9 Jan 2020

--- src/main/java/com/preflex/AirlineProblem.java
+++ src/main/java/com/preflex/AirlineProblem.java
@@ -2,37 +2,41 @@ package com.preflex;
 /**
  * Java Class to Determine if airlines can be moved from
  * airline to another based on network of airline partners
+ *
  * @author
  */
-import java.util.ArrayList;
-import java.util.Scanner;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AirlineProblem {
 
-    public static void main(String[] args){
+
+    //public static void main(String[] args)
+    public static void main(String[] args) {
+
+    }{
         Scanner scannerToReadAirlines = null;
-        try{
+        try {
 
             scannerToReadAirlines = new Scanner(new File("airlines.txt"));
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Could not connect to file airlines.txt.");
             System.exit(0);
         }
-        if(scannerToReadAirlines != null){
+        if (scannerToReadAirlines != null) {
             ArrayList<Airline> airlinesPartnersNetwork = new ArrayList<Airline>();
             Airline newAirline;
             String lineFromFile;
             String[] airlineNames;
 
-            while( scannerToReadAirlines.hasNext() ){
+            while (scannerToReadAirlines.hasNext()) {
                 lineFromFile = scannerToReadAirlines.nextLine();
                 airlineNames = lineFromFile.split(",");
                 newAirline = new Airline(airlineNames);
-                airlinesPartnersNetwork.add( newAirline );
+                airlinesPartnersNetwork.add(newAirline);
             }
             System.out.println(airlinesPartnersNetwork);
             Scanner keyboard = new Scanner(System.in);
@@ -43,26 +47,26 @@ public class AirlineProblem {
             String goal = keyboard.nextLine();
             ArrayList<String> pathForMiles = new ArrayList<String>();
             ArrayList<String> airlinesVisited = new ArrayList<String>();
-            if( canRedeem(start, goal, pathForMiles, airlinesVisited, airlinesPartnersNetwork))
+            if (canRedeem(start, goal, pathForMiles, airlinesVisited, airlinesPartnersNetwork))
                 System.out.println("Path to redeem miles: " + pathForMiles);
             else
                 System.out.println("Cannot convert miles from " + start + " to " + goal + ".");
+
         }
     }
 
     private static boolean canRedeem(String current, String goal,
                                      ArrayList<String> pathForMiles, ArrayList<String> airlinesVisited,
-                                     ArrayList<Airline> network){
-        if(current.equals(goal)){
+                                     ArrayList<Airline> network) {
+        if (current.equals(goal)) {
             //base case 1, I have found a path!
             pathForMiles.add(current);
             return true;
-        }
-        else if(airlinesVisited.contains(current))
+        } else if (airlinesVisited.contains(current))
             // base case 2, I have already been here
             // don't go into a cycle
             return false;
-        else{
+        else {
             // I have not been here and it isn't
             // the goal so check its partners
             // now I have been here
@@ -74,55 +78,55 @@ public class AirlineProblem {
             // find this airline in the network
             int pos = -1;
             int index = 0;
-            while(pos == -1 && index < network.size()){
-                if(network.get(index).getName().equals(current))
+            while (pos == -1 && index < network.size()) {
+                if (network.get(index).getName().equals(current))
                     pos = index;
                 index++;
             }
             //if not in the network, no partners
-            if( pos == - 1)
+            if (pos == -1)
                 return false;
 
             // loop through partners
             index = 0;
             String[] partners = network.get(pos).getPartners();
             boolean foundPath = false;
-            while( !foundPath && index < partners.length){
+            while (!foundPath && index < partners.length) {
                 foundPath = canRedeem(partners[index], goal, pathForMiles, airlinesVisited, network);
                 index++;
             }
-            if( !foundPath )
-                pathForMiles.remove( pathForMiles.size() - 1);
+            if (!foundPath)
+                pathForMiles.remove(pathForMiles.size() - 1);
             return foundPath;
         }
     }
 
-    private static class Airline{
+    private static class Airline {
         private String name;
         private ArrayList<String> partners;
 
         //pre: data != null, data.length > 0
-        public Airline(String[] data){
+        public Airline(String[] data) {
             assert data != null && data.length > 0 : "Failed precondition";
             name = data[0];
             partners = new ArrayList<String>();
-            for(int i = 1; i < data.length; i++)
-                partners.add( data[i] );
+            for (int i = 1; i < data.length; i++)
+                partners.add(data[i]);
         }
 
-        public String[] getPartners(){
+        public String[] getPartners() {
             return partners.toArray(new String[partners.size()]);
         }
 
-        public boolean isPartner(String name){
+        public boolean isPartner(String name) {
             return partners.contains(name);
         }
 
-        public String getName(){
+        public String getName() {
             return name;
         }
 
-        public String toString(){
+        public String toString() {
             return name + ", partners: " + partners;
         }
     }
